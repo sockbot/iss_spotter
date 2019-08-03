6ef6af1f51566a2b7235605f2ab89960ba00d486
sockbot <github@sockbot.com>
2019-08-03 02:50:35 +0000

    bypassed fetchCoordsByIP API failure with hardcoded coords

--- iss_promised.js
+++ iss_promised.js
@@ -5,13 +5,19 @@ const fetchMyIP = function() {
 }
 
 const fetchCoordsByIP = function(body) {
+  const latLong = '{ "data": { "latitude": 49.26200, "longitude": -123.09230 } }';
   const ip = JSON.parse(body).ip;
-  return request("https://ipvigilante.com/" + ip)
+  // return request("https://ipvigilante.com/" + ip)
+  return new Promise((resolve, reject) => {
+    resolve(latLong);
+  })
 };
 
 const fetchISSFlyOverTimes = function(body) {
-  const jsonString = JSON.parse(body);
+  // console.log(typeof body)
   let coords = {}
+  const jsonString = JSON.parse(body);
+  // console.log(typeof jsonString);
   coords.latitude = jsonString.data.latitude;
   coords.longitude = jsonString.data.longitude;
   return request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`)
